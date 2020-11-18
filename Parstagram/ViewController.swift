24084025bc7f574a6eabbfcc1855ac017bb7804a
shreeyesh <shreeyeshc@gmail.com>
2020-11-18 11:10:58 -0600

    Initial Commit

---
+++ Parstagram/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Parstagram
+//
+//  Created by Shreeyesh Chauhan on 11/18/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
