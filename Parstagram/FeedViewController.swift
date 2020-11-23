20f0ba9473f9cca856fc78d9cfe9916148be09dc
shreeyesh <shreeyeshc@gmail.com>
2020-11-23 13:15:19 -0600

    Initial

---
+++ Parstagram/FeedViewController.swift
@@ -0,0 +1,96 @@
+ //
+//  FeedViewController.swift
+//  Parstagram
+//
+//  Created by Shreeyesh Chauhan on 11/18/20.
+//
+
+import UIKit
+import Parse
+import AlamofireImage
+
+ class FeedViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    
+    //var window: UIWindow?
+    
+    let myReloadControl = UIRefreshControl()
+    
+    @IBOutlet weak var tableView: UITableView!
+    
+    var posts = [PFObject]()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+       // myReloadControl.addTarget(self, action: #selector(posts), for: .valueChanged)
+        self.tableView.refreshControl = myReloadControl
+        tableView.delegate = self
+        tableView.dataSource = self
+        // Do any additional setup after loading the view.
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        
+        let query = PFQuery(className: "Posts")
+        query.includeKey("author")
+        query.limit = 30
+        
+        query.findObjectsInBackground { (posts, error) in
+            if posts != nil {
+                self.posts = posts!
+                self.tableView.reloadData()
+            }
+            
+        }
+        
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return posts.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        let cell = tableView.dequeueReusableCell(withIdentifier: "PostCell") as! PostCell
+        let post = posts[indexPath.row]
+        
+        let user = post["author"] as! PFUser
+        cell.usernameLabel.text = user.username
+        
+        cell.captionLabel.text = post["caption"] as! String
+        
+        let imageFile = post["image"] as! PFFileObject
+        let urlString = imageFile.url!
+        let url = URL(string: urlString)!
+        
+        cell.photoView.af.setImage(withURL: url)
+        
+        return cell
+        
+    }
+    
+    
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+    
+    
+    @IBAction func onLogoutButton(_ sender: Any) {
+        PFUser.logOut()
+        
+        let main = UIStoryboard(name: "Main", bundle: nil)
+        let loginViewController = main.instantiateViewController(withIdentifier: "LoginViewController")
+        
+        //let delgete = UIApplication.shared.delegate as! AppDelegate
+        let delegete = self.view.window?.windowScene?.delegate as! SceneDelegate
+        delegete.window?.rootViewController = loginViewController
+    }
+    
+}
